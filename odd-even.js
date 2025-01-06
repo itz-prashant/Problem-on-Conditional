4bc4a38f8d3b36d5feaf6cdddc04929d57e8634c
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-01-06 11:25:11 +0530

    Add odd-even conditional

---
+++ odd-even.js
@@ -0,0 +1,10 @@
+const printOddEven = function(x){
+    if (x % 2 == 0) {
+        return "Even"
+    } else {
+        return "Odd"
+    }
+}
+
+console.log(printOddEven(11)); // output odd
+console.log(printOddEven(10)); // output even
