6ae0c0d53202fcc670f32085e117c135a773a9a5
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-01-06 11:48:29 +0530

    Find minimum value among 3 input

---
+++ find-min-value.js
@@ -0,0 +1,13 @@
+let a = 8;
+let b = 5;
+let c = 3;
+
+if (a<b && a<c ){
+    console.log(a);
+}else if(b<a && b<c){
+    console.log(b);
+}else if(c<a && c<b){
+    console.log(c);
+}else{
+ console.log("Some Value are equall");
+}
